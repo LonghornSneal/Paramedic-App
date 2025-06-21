471b44d0373088ae03e71bb50e5fb4bbd247880b
LonghornSneal <jlawhon107@gmail.com>
2025-06-21 08:28:52 -0500

    updates

---
+++ slugAnchors.js
@@ -0,0 +1,14 @@
+    <script>
+      if (typeof slugIDs !== 'undefined') {
+        const container = document.createElement('div');
+        container.id = 'slug-id-container';
+        container.classList.add('hidden');
+        slugIDs.forEach(id => {
+          const div = document.createElement('div');
+          div.id = id;
+          div.dataset.branch = id;
+          container.appendChild(div);
+        });
+        document.body.appendChild(container);
+      }
+    </script>
