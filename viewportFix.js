c2fdf1a792c118fdc142c9e6dbede48325e49d66
LonghornSneal <jlawhon107@gmail.com>
2025-06-22 10:07:03 -0500

    removed html script tags

--- viewportFix.js
+++ viewportFix.js
@@ -1,8 +1,6 @@
-<script>
   function setVh() {
     const vh=window.innerHeight*0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
   }
   window.addEventListener("resize", setVh);
   setVh();
-</script>
