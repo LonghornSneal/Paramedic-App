d946c2a39751af3785005f76c171d7b17674c07b
LonghornSneal <jlawhon107@gmail.com>
2025-06-24 22:54:51 -0500

    dfs

--- Data/ParamedicCategoriesData2.js
+++
@@ -1,5 +0,0 @@
-// Data/ParamedicCategoriesData.js
-const paramedicCategoriesData = [
-];
-if (typeof module !== 'undefined') module.exports = paramedicCategoriesData;
-if (typeof window !== 'undefined') window.paramedicCategoriesData = paramedicCategoriesData;
