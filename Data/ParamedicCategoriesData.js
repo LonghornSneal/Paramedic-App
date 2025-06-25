0d44619377bcbbefd4f864895b14b0b76aaeff12
LonghornSneal <jlawhon107@gmail.com>
2025-06-24 23:00:01 -0500

    kj

--- Data/ParamedicCategoriesData.js
+++ Data/ParamedicCategoriesData.js
@@ -2,4 +2,4 @@
 const paramedicCategoriesData = [goofy shit
 ];
 if (typeof module !== 'undefined') module.exports = paramedicCategoriesData;
-if (typeof window !== 'undefined') window.paramedicCategoriesData = paramedicCategoriesData;
+if (typeof window !== 'undefined') window.ParamedicCategoriesData = paramedicCategoriesData;
